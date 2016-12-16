41167bbc92c23ba8fd4423de8e091910f093ee3c
zyddora <zyddora@gmail.com>
2016-12-16 11:02:37 +0800

    rectCircleAreaUseIntf.go

---
+++ rectCircleAreaUseIntf.go
@@ -0,0 +1,52 @@
+/*	Date & Author: Orchid Zhang 20161216
+ *	Description: calculate rectangle and circle area
+ *				using Interface 
+ */
+
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	
+	rect := Rectangle{20, 50}
+	circ := Circle{4}
+
+	fmt.Println("Rectangle Area =", getArea(rect))
+	fmt.Println("Circle Area =", getArea(circ))
+
+}
+
+// interface
+type Shape interface {
+
+	area() float64
+}
+
+type Rectangle struct {
+
+	height float64
+	width float64
+}
+
+type Circle struct {
+
+	radius float64
+}
+
+
+func (r Rectangle) area() float64{ 
+	
+	return r.width * r.height
+}
+
+func (c Circle) area() float64{ 
+	
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func getArea(shape Shape) float64{
+	
+	return shape.area()
+}
